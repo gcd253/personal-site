40acebfd92191b475079a65f0642b38716ef62ca
Griffin <dooleygriffin@gmail.com>
2023-02-11 19:15:45 -0500

    footer started

--- components/Layout.js
+++ components/Layout.js
@@ -1,39 +1,42 @@
 const Layout = ({ children }) => {
   return (
-    <div>
+    <>
       <div className="h-20 w-full items-center justify-center flex gap-x-20">
         <p className="m-16 relative group cursor-pointer p-x-1">
           <span>
-            Navbar things
+            home
             <span className="absolute -bottom-1 left-1/2 w-0 h-1 bg-white group-hover:w-1/2 group-hover:transition-all duration-500"></span>
             <span className="absolute -bottom-1 right-1/2 w-0 h-1 bg-white group-hover:w-1/2 group-hover:transition-all duration-500"></span>
           </span>
         </p>
         <p className="m-16 relative group cursor-pointer p-x-1">
           <span>
-            Navbar things
+            about me
             <span className="absolute -bottom-1 left-1/2 w-0 h-1 bg-white group-hover:w-1/2 group-hover:transition-all duration-500"></span>
             <span className="absolute -bottom-1 right-1/2 w-0 h-1 bg-white group-hover:w-1/2 group-hover:transition-all duration-500"></span>
           </span>
         </p>
         <p className="m-16 relative group cursor-pointer p-x-1">
           <span>
-            Navbar things
+            portfolio
             <span className="absolute -bottom-1 left-1/2 w-0 h-1 bg-white group-hover:w-1/2 group-hover:transition-all duration-500"></span>
             <span className="absolute -bottom-1 right-1/2 w-0 h-1 bg-white group-hover:w-1/2 group-hover:transition-all duration-500"></span>
           </span>
         </p>
         <p className="m-16 relative group cursor-pointer p-x-1">
           <span>
-            Navbar things
+            resume
             <span className="absolute -bottom-1 left-1/2 w-0 h-1 bg-white group-hover:w-1/2 group-hover:transition-all duration-500"></span>
             <span className="absolute -bottom-1 right-1/2 w-0 h-1 bg-white group-hover:w-1/2 group-hover:transition-all duration-500"></span>
           </span>
         </p>
       </div>
       {children}
-      <footer></footer>
-    </div>
+      <footer className="h-60 w-[90%] items-center m-auto justify-center flex gap-x-20 border-slate-600 border-t">
+        <div className="m-20">Github</div>
+        <div className="m-20">LinkedIn</div>
+      </footer>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import headshot from '../public/headshot-square.PNG';
+import headshot from "../public/headshot-square.PNG";
 import Layout from "@/components/Layout";
-
 
 export default function Home() {
   return (
@@ -15,20 +14,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-      <main className={styles.main}>
+        <main className={styles.main}>
+          <Image
+            src={headshot}
+            alt="Picture of the author"
+            width={500}
+            height={500}
+            className="rounded-full border-4 border-white hover:scale-[1.03] duration-500"
+          />
 
-        <Image
-          src={headshot}
-          alt="Picture of the author"
-          width={500}
-          height={500}
-          className="rounded-full border-4 border-white hover:scale-[1.03] duration-500"
-        />
-
-        <div className="text-white bold font text-5xl font-serif">
-          Hi, I'm Griffin
-        </div>
-      </main>
+          <div className="text-white bold font text-5xl font-serif">
+            Hi, I'm Griffin
+          </div>
+        </main>
       </Layout>
     </>
   );
