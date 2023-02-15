e1369f584b62d4effb6992b2725afe999c60da03
Griffin <dooleygriffin@gmail.com>
2023-02-14 20:18:59 -0500

    more sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,11 +23,27 @@ export default function Home() {
                   <div className="bg-blue-400 h-4 w-4 rounded-full"></div>
                 </div>
               </div>
-
               <div className="absolute bg-purple-400 h-4 w-12 -right-16"></div>
               <div className="absolute bg-purple-400 h-4 w-12 -right-[3.2rem] -top-12 -rotate-45"></div>
               <div className="absolute bg-purple-400 h-12 w-4 right-4 -top-20"></div>
+            </div>
+          </div>
 
+          {/* second section */}
+
+          <div className="flex flex-col items-center justify-center h-screen w-screen bg-fuchsia-300">
+            <div className="mt-12 h-96 w-1/3 bg-black rounded-md relative">
+              <div className="h-[101%] w-[101%] bg-white border-[6px] border-black items-center -top-3 -left-3 rounded-md absolute flex justify-center text-3xl p-12">
+                Hi, I'm Griffin! What if I just put way too much text in this tiny little box? Will it look awful and cramped? Yeah, kinda.
+                <div className="absolute flex flex-row gap-2 bottom-2 left-2">
+                  <div className="bg-red-400 h-4 w-4 rounded-full"></div>
+                  <div className="bg-yellow-400 h-4 w-4 rounded-full"></div>
+                  <div className="bg-green-400 h-4 w-4 rounded-full"></div>
+                </div>
+              </div>
+              <div className="absolute bg-teal-500 h-4 w-12 -right-16"></div>
+              <div className="absolute bg-teal-500 h-4 w-12 -right-[3.2rem] -top-12 -rotate-45"></div>
+              <div className="absolute bg-teal-500 h-12 w-4 right-4 -top-20"></div>
             </div>
           </div>
         </main>
